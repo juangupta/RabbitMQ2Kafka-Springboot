d520533c7d495b45aabdda3a2d32bb734a136f77
juagomez <juagomez@192.168.99.1>
2018-09-04 13:08:06 -0500

    c2

--- src/main/java/com/javasampleapproach/rabbitmq/SpringRabbitMqSubcriberApplication.java
+++ src/main/java/com/javasampleapproach/rabbitmq/SpringRabbitMqSubcriberApplication.java
@@ -20,8 +20,8 @@ public class SpringRabbitMqSubcriberApplication {
         CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
         //connectionFactory.setUri("amqp://juagomez:vinula@localhost:5672");
     	//*******RabbitMQ-DEV
-        connectionFactory.setUri("amqps://admin:SNMYSCACWOLMHZHS@portal-ssl1233-20.bmix-dal-yp-eccd01e7-4f3d-4c90-bc67-220feeeb8e46.2126222060.composedb.com:54907/bmix-dal-yp-eccd01e7-4f3d-4c90-bc67-220feeeb8e46");
-        //connectionFactory.setUri("amqp://ifyrtjsc:F9fttMFgJ1_RUJX6d4DIvn9TyQiNmiR8@chimpanzee.rmq.cloudamqp.com/ifyrtjsc");
+        //connectionFactory.setUri("amqps://admin:SNMYSCACWOLMHZHS@portal-ssl1233-20.bmix-dal-yp-eccd01e7-4f3d-4c90-bc67-220feeeb8e46.2126222060.composedb.com:54907/bmix-dal-yp-eccd01e7-4f3d-4c90-bc67-220feeeb8e46");
+        connectionFactory.setUri("amqp://ifyrtjsc:F9fttMFgJ1_RUJX6d4DIvn9TyQiNmiR8@chimpanzee.rmq.cloudamqp.com/ifyrtjsc");
     	//connectionFactory.setCacheMode(CacheMode.CONNECTION);
         //connectionFactory.setUsername("guest");
         //connectionFactory.setPassword("guest");
