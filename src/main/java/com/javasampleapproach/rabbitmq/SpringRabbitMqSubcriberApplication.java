ea2e2cf37979724a9561dacd9a530dd9273ce678
juagomez <juagomez@192.168.99.1>
2018-09-12 11:15:03 -0500

    Commit Suscriber

--- src/main/java/com/javasampleapproach/rabbitmq/SpringRabbitMqSubcriberApplication.java
+++ src/main/java/com/javasampleapproach/rabbitmq/SpringRabbitMqSubcriberApplication.java
@@ -21,7 +21,7 @@ public class SpringRabbitMqSubcriberApplication {
         //connectionFactory.setUri("amqp://juagomez:vinula@localhost:5672");
     	//*******RabbitMQ-DEV
         //connectionFactory.setUri("amqps://admin:SNMYSCACWOLMHZHS@portal-ssl1233-20.bmix-dal-yp-eccd01e7-4f3d-4c90-bc67-220feeeb8e46.2126222060.composedb.com:54907/bmix-dal-yp-eccd01e7-4f3d-4c90-bc67-220feeeb8e46");
-        connectionFactory.setUri("amqp://ifyrtjsc:F9fttMFgJ1_RUJX6d4DIvn9TyQiNmiR8@chimpanzee.rmq.cloudamqp.com/ifyrtjsc");
+        connectionFactory.setUri("amqp://kqeniqmj:9IX8D-nhTOJRQo643vNuHeJwvwdb-NYw@fly.rmq.cloudamqp.com/kqeniqmj");
     	//connectionFactory.setCacheMode(CacheMode.CONNECTION);
         //connectionFactory.setUsername("guest");
         //connectionFactory.setPassword("guest");
@@ -32,8 +32,8 @@ public class SpringRabbitMqSubcriberApplication {
 	public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
 	    SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
 	    factory.setConnectionFactory(connectionFactory());
-	    factory.setConcurrentConsumers(50);
-	    factory.setMaxConcurrentConsumers(1000);
+	    //factory.setConcurrentConsumers(50);
+	    //factory.setMaxConcurrentConsumers(1000);
 	    return factory;
 	}
 }
--- src/main/java/com/javasampleapproach/rabbitmq/subcriber/Subcriber.java
+++ src/main/java/com/javasampleapproach/rabbitmq/subcriber/Subcriber.java
@@ -1,7 +1,6 @@
 package com.javasampleapproach.rabbitmq.subcriber;
 
 import java.sql.Timestamp;
-import java.util.concurrent.TimeUnit;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -12,12 +11,6 @@ public class Subcriber {
 	@RabbitListener(queues="${jgg.rabbitmq.queue}", containerFactory="rabbitListenerContainerFactory")
     //public String recievedMessage(String msg) {
 		public void recievedMessage(String msg) {
-		try {
-			Thread.sleep(600);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
         System.out.println(new Timestamp(System.currentTimeMillis()) + " - Recieved Message: " + msg);
         //return (msg);
     }
