2c3eee7099975712ddf6099b55b114d3c2789227
juagomez <juagomez@localhost>
2019-06-18 10:07:26 -0500

    Commit ack

--- src/main/java/com/javasampleapproach/rabbitmq/SpringRabbitMqSubcriberApplication.java
+++ src/main/java/com/javasampleapproach/rabbitmq/SpringRabbitMqSubcriberApplication.java
@@ -1,8 +1,8 @@
 package com.javasampleapproach.rabbitmq;
 
+import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory.CacheMode;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,8 +21,8 @@ public class SpringRabbitMqSubcriberApplication {
         //connectionFactory.setUri("amqp://juagomez:vinula@localhost:5672");
     	//*******RabbitMQ-DEV
         //connectionFactory.setUri("amqps://admin:SNMYSCACWOLMHZHS@portal-ssl1233-20.bmix-dal-yp-eccd01e7-4f3d-4c90-bc67-220feeeb8e46.2126222060.composedb.com:54907/bmix-dal-yp-eccd01e7-4f3d-4c90-bc67-220feeeb8e46");
-        connectionFactory.setUri("amqp://kqeniqmj:9IX8D-nhTOJRQo643vNuHeJwvwdb-NYw@fly.rmq.cloudamqp.com/kqeniqmj");
-    	//connectionFactory.setCacheMode(CacheMode.CONNECTION);
+        connectionFactory.setUri("amqp://ifyrtjsc:F9fttMFgJ1_RUJX6d4DIvn9TyQiNmiR8@chimpanzee.rmq.cloudamqp.com/ifyrtjsc");
+        //connectionFactory.setCacheMode(CacheMode.CONNECTION);
         //connectionFactory.setUsername("guest");
         //connectionFactory.setPassword("guest");
         return connectionFactory;
@@ -32,7 +32,8 @@ public class SpringRabbitMqSubcriberApplication {
 	public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
 	    SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
 	    factory.setConnectionFactory(connectionFactory());
-	    //factory.setConcurrentConsumers(50);
+	    factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+	    factory.setConcurrentConsumers(10);
 	    //factory.setMaxConcurrentConsumers(1000);
 	    return factory;
 	}
--- src/main/java/com/javasampleapproach/rabbitmq/subcriber/Subcriber.java
+++ src/main/java/com/javasampleapproach/rabbitmq/subcriber/Subcriber.java
@@ -1,17 +1,30 @@
 package com.javasampleapproach.rabbitmq.subcriber;
 
+import java.io.IOException;
 import java.sql.Timestamp;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+
+import com.rabbitmq.client.Channel;
+
 
 @Component
 public class Subcriber {
 
 	@RabbitListener(queues="${jgg.rabbitmq.queue}", containerFactory="rabbitListenerContainerFactory")
     //public String recievedMessage(String msg) {
-		public void recievedMessage(String msg) {
+		public void recievedMessage(byte[] bytes, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
+		String msg = new String(bytes);
         System.out.println(new Timestamp(System.currentTimeMillis()) + " - Recieved Message: " + msg);
-        //return (msg);
+        if (msg.equalsIgnoreCase("error")) {
+			channel.basicNack(tag, false, true);
+        }
+        else {
+			channel.basicAck(tag, false);
+        }
+        //return (new Timestamp(System.currentTimeMillis()) + " - Recieved Message: " + msg);
     }
 }
